1c496570ce4ba9d9e4b7abbe08a08e6407dad5d3
Randall.Xia <narutonbm@gmail.com>
2023-09-12 23:01:35 +0800

    step 6-7

--- app/src/main/java/com/randalldev/myapplication/MainActivity.kt
+++ app/src/main/java/com/randalldev/myapplication/MainActivity.kt
@@ -3,13 +3,21 @@ package com.randalldev.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.randalldev.myapplication.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +26,58 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearnComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String) {
+
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+
+    val expandPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = expandPadding)
+            ) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(text = if (expanded.value) "more" else "less")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LearnComposeTheme {
-        Greeting("Android")
+        MyApp(modifier = Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("Android", "Kotlin")) {
+    Column(modifier = modifier.padding(4.dp)) {
+        names.forEach {
+            Greeting(name = it)
+        }
     }
 }
