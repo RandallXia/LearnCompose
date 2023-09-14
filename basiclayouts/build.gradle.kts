3a39fda058f5ef4ebde644207b9d8e7dbbf2aa1f
Randall.Xia <narutonbm@gmail.com>
2023-09-14 23:15:03 +0800

    add lab2 BasicLayouts

--- basiclayouts/build.gradle.kts
+++ basiclayouts/build.gradle.kts
@@ -1,18 +1,15 @@
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.randalldev.myapplication"
+    namespace = "com.randalldev.basiclayoutscodelab"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.randalldev.myapplication"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -44,18 +41,13 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    lint {
-        baseline = file("lint-baseline.xml")
-    }
 }
 
 dependencies {
 
-    implementation(project(mapOf("path" to ":basiclayouts")))
-
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
