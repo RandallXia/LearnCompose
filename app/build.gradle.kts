25f8eef17a2b58b03817017ba2c8974ab9a7db6f
Randall.Xia <narutonbm@gmail.com>
2024-04-30 23:30:36 +0800

    config: upgrade compose dependence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,12 @@ plugins {
 }
 
 android {
-    namespace = "com.randalldev.myapplication"
-    compileSdk = 33
+    namespace = "com.randalldev.learncompose"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.randalldev.myapplication"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -54,9 +53,9 @@ dependencies {
     implementation(project(mapOf("path" to ":basiclayouts")))
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -64,7 +63,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
