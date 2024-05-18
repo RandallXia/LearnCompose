f0a189780f0e23e053af4622aea1eddfed40dfc6
Randall.Xia <narutonbm@gmail.com>
2024-05-19 00:08:40 +0800

    feat: step 3 - 7

--- state/src/main/java/com/randalldev/state/WellnessScreen.kt
+++ state/src/main/java/com/randalldev/state/WellnessScreen.kt
@@ -15,27 +15,10 @@
  */
 package com.randalldev.state
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun WellnessScreen(
-    modifier: Modifier = Modifier,
-    wellnessViewModel: WellnessViewModel = viewModel()
-) {
-    Column(modifier = modifier) {
-        StatefulCounter()
-
-        WellnessTasksList(
-            list = wellnessViewModel.tasks,
-            onCheckedTask = { task, checked ->
-                wellnessViewModel.changeTaskChecked(task, checked)
-            },
-            onCloseTask = { task ->
-                wellnessViewModel.remove(task)
-            }
-        )
-    }
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
 }
