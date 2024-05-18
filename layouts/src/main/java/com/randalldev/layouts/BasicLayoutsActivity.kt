e0764a40b6d9fae975d7f991cb5d5a26d995f5e9
Randall.Xia <narutonbm@gmail.com>
2024-05-19 00:08:40 +0800

    feat: step 3 - 7

--- layouts/src/main/java/com/randalldev/layouts/BasicLayoutsActivity.kt
+++ layouts/src/main/java/com/randalldev/layouts/BasicLayoutsActivity.kt
@@ -22,6 +22,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
--- state/src/main/java/com/randalldev/state/WaterCounter.kt
+++ state/src/main/java/com/randalldev/state/WaterCounter.kt
@@ -21,34 +21,27 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun StatefulCounter(modifier: Modifier = Modifier) {
-    var count by rememberSaveable { mutableStateOf(0) }
-    StatelessCounter(
-        count = count,
-        onIncrement = { count++ },
-        modifier = modifier
-    )
-}
-
-@Composable
-fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
+
+        var count by remember { mutableIntStateOf(0) }
+
         if (count > 0) {
-            Text("You've had $count glasses.")
+            Text(text = "You've had $count glasses.")
         }
         Button(
-            onClick = onIncrement,
-            enabled = count < 10,
-            modifier = Modifier.padding(top = 8.dp)
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
         ) {
-            Text("Add one")
+            Text(text = "Add one")
         }
     }
 }
