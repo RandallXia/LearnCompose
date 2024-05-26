18d9750a693c9a46316e431378e9e8e5dc99555e
Randall.Xia <narutonbm@gmail.com>
2024-05-26 22:52:26 +0800

    feat: step 5 - 9

--- migration/src/main/java/com/randalldev/sunflower/plantdetail/PlantDetailDescription.kt
+++ migration/src/main/java/com/randalldev/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,120 @@
 
 package com.randalldev.sunflower.plantdetail
 
+import android.content.Context
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+import com.randalldev.sunflower.R
+import com.randalldev.sunflower.data.Plant
+import com.randalldev.sunflower.viewmodels.PlantDetailViewModel
 
 @Composable
-fun PlantDetailDescription() {
+fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
+    val plant by plantDetailViewModel.plant.observeAsState()
+
+    plant?.let {
+        PlantDetailContent(plant = it)
+    }
+}
+
+@Composable
+fun PlantDescription(description: String) {
+    val htmlDesc = remember(description) {
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+
+    AndroidView(
+        factory = { context: Context ->
+            TextView(context).apply {
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+        },
+        update = {
+            it.text = htmlDesc
+        }
+    )
+}
+
+@Composable
+fun PlantDetailContent(plant: Plant) {
     Surface {
-        Text("Hello Compose")
+        Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.margin_normal))) {
+            PlantName(name = plant.name)
+            PlantWatering(wateringInterval = plant.wateringInterval)
+            PlantDescription(description = plant.description)
+        }
+    }
+}
+
+@Composable
+fun PlantWatering(wateringInterval: Int) {
+    Column {
+        val centerWithPaddingModifier = Modifier
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .align(Alignment.CenterHorizontally)
+
+        val normalPadding = dimensionResource(id = R.dimen.margin_normal)
+
+        Text(
+            text = stringResource(id = R.string.watering_needs_prefix),
+            color = MaterialTheme.colors.primaryVariant,
+            fontWeight = FontWeight.Bold,
+            modifier = centerWithPaddingModifier.padding(top = normalPadding)
+        )
+
+        val waterIntervalText =
+            pluralStringResource(id = R.plurals.watering_needs_suffix, count = wateringInterval, wateringInterval)
+        Text(
+            text = waterIntervalText,
+            modifier = centerWithPaddingModifier.padding(bottom = normalPadding)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PlantWateringPreview(modifier: Modifier = Modifier) {
+    MaterialTheme {
+        PlantWatering(wateringInterval = 7)
+    }
+}
+
+@Composable
+private fun PlantName(name: String) {
+    Text(
+        text = name,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
+@Preview
+@Composable
+fun PlantNamePreview(modifier: Modifier = Modifier) {
+    MaterialTheme {
+        PlantName(name = "Apple")
     }
 }
